c58a505c1eb137e8dc228919d746c9560df949b6
Francois Szczepkowski <francois.szcz@hotmail.fr>
2023-11-03 13:48:05 +0100

    auth: mise en place du système de login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { Op } = require("sequelize");
 
 exports.getAllUsers = async (req, res) => {
     try {
@@ -33,5 +35,42 @@ exports.registerUser = async (req, res) => {
         res.status(201).send({ auth: true, token });
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { login, MotDePasse } = req.body; // login peut être soit l'email, soit le pseudo
+
+        // Trouvez l'utilisateur par email ou pseudo
+        const user = await User.findOne({
+            where: {
+                [Op.or]: [{ Email: login }, { Pseudo: login }],
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé." });
+        }
+
+        // Vérifiez le mot de passe
+        const passwordIsValid = bcrypt.compareSync(MotDePasse, user.MotDePasse);
+        if (!passwordIsValid) {
+            return res.status(401).json({
+                auth: false,
+                token: null,
+                message: "Mot de passe invalide.",
+            });
+        }
+
+        // Si le mot de passe est correct, générez un token
+        const token = jwt.sign({ id: user.UserID }, process.env.JWT_SECRET, {
+            expiresIn: 86400, // expire en 24 heures
+        });
+
+        // Répondez avec un token
+        res.status(200).json({ auth: true, token });
+    } catch (error) {
+        res.status(500).json({ message: "Il y a eu un problème à se connecter." });
     }
 };
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser } = require("../controllers/user.controller");
+const { loginUser } = require("../controllers/user.controller");
 
 // POST : Inscription
 router.post("/register", registerUser);
 
 // POST : Connexion
-router.post("/login", (req, res) => {
-	// Logique de connexion
-});
+router.post("/login", loginUser);
 
 // GET : Profil de l'utilisateur
 router.get("/profile", (req, res) => {
-	// Logique pour récupérer le profil de l'utilisateur
+    // Logique pour récupérer le profil de l'utilisateur
 });
 
 module.exports = router;
